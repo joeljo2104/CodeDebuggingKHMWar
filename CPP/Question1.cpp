518ff958119289879029bd5d528fed55955f6ca3
Joel <joeljo2104@gmail.com>
2020-04-06 22:23:02 +0530

    Update V3

--- CPP/Question1.cpp
+++ CPP/Question1.cpp
@@ -1,59 +1,51 @@
-
 #include <iostream>
-#include <stirng.h>
+#include <string.h> // string spelling
+using namespace std;
+int getLIndex(char[], char); // added prototype
+int getFIndex(char[], char);
 int main()
 {
 	char str[100];
-	char ch; 
-	int Lindex,Findex; 
-	cin>>str
-	cin>>ch;
-	Lindex = getLIndex(str,ch);
-	Findex= getFIndex(str,ch);
-	if(Lindex!=-1)
-		cout<<Findex<<endl<<Lindex;
+	char ch;
+	int Lindex, Findex;
+	cin >> str; // semi colon
+	cin >> ch;
+	Lindex = getLIndex(str, ch);
+	Findex = getFIndex(str, ch);
+	if (Lindex != -1)
+		cout << Findex << endl
+			 << Lindex;
 	else
-		cout<<"NOT FOUND"<<endl;
-	return 0;	
+		cout << "NOT FOUND" << endl;
+	return 0;
 }
-int getLIndex(char string,char  c)
+int getFIndex(char string[], char c) // square brackets and spelling changed
 {
-	int size = len(string),i=0;
-	while(i<size)
+	int size = strlen(string); // strlen function
+	int i = 0;
+	while (i < size)
 	{
-		
-		if(string[i]=c){
-		    return i;
-		   break;
-		}	
-		i++; 
+		if (string[i] == c)
+		{
+			return i;
+			break;
+		}
+		i++;
 	}
-	
 }
-int getFindex(string, c)
+int getLIndex(char string[], char c) // square brackets and spelling changed
 {
-	int size = len(string);
-	int i=size; 
-	while( i>=0);
-{		
-		if(string[i]==c){
-		    return i;
- break;
-		}	
-	i--;
+	{
+		int size = strlen(string); // strlen function
+		int i = size;
+		while (i >= 0) // removed semicolon
+		{
+			if (string[i] == c)
+			{
+				return i;
+				break;
+			}
+			i--;
+		}
+	}
 }
-		
-}
-SAMPLE INPUT-1
-	Kerala
-a
-SAMPLE OUTPUT:1
-3
-5
-SAMPLE INPUT-2
-MALAYALaM
-A
-
-SAMPLE OUTPUT:2
-1
-5  
